93138a4bc180a3b656f9795e9a581781e4d2843c
SonnySky75 <85204037+SonnySky75@users.noreply.github.com>
2021-09-11 12:33:34 -0500

    created ContactForm.js

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,5 +1,7 @@
 import React from "react"
+
 import ServiceCards from "./SerivceCards"
+import ContactForm from "./ContactForm"
 
 function ContactUs() {
   const serviceCards = [
@@ -34,46 +36,9 @@ function ContactUs() {
             <h1 className="page-title">Services</h1>
             <ServiceCards serviceCards={serviceCards} />
           </article>
-          <aside id="sidebar">
-            <div className="dark">
-              <h3>Contact Us</h3>
-              <form className="contactForm">
-                <div>
-                  <label>First Name</label>
-                  <br />
-                  <input type="text" placeholder="first name" />
-                </div>
-                <div>
-                  <label>Last Number</label>
-                  <br />
-                  <input type="text" placeholder="last name" />
-                </div>
-                <div>
-                  <label>Email</label>
-                  <br />
-                  <input type="email" placeholder="Emial Address" />
-                </div>
-                <div>
-                  <label>Phone Number</label>
-                  <br />
-                  <input type="number" placeholder="Phone Number" />
-                </div>
-                <div>
-                  <label>Address</label>
-                  <br />
-                  <textarea placeholder="address"></textarea>
-                </div>
-                <div>
-                  <label>Message</label>
-                  <br />
-                  <textarea placeholder="Message"></textarea>
-                </div>
-                <button className="button" type="submit">
-                  Send
-                </button>
-              </form>
-            </div>
-          </aside>
+
+          <ContactForm />
+          
         </div>
       </section>
     </>
